f6466ba41caf1b0641746646ebeec28b720d7cd1
Sathwik Reddy <sathwikreddy@Mac.attlocal.net>
2025-08-19 20:21:47 -0400

    Fix build errors by removing deleted component references

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,13 +8,8 @@ import UploadPage from './pages/UploadPage';
 import BrowseAgentsPage from './pages/BrowseAgentsPage';
 import AgentDetailPage from './pages/AgentDetailPage';
 import NotFoundPage from './pages/NotFoundPage';
-import ApiExample from './components/ApiExample';
-import AgentCardDemo from './components/AgentCardDemo';
-import CategoryFilterDemo from './components/CategoryFilterDemo';
-import FilterIntegrationDemo from './components/FilterIntegrationDemo';
-import DownloadFunctionalityDemo from './components/DownloadFunctionalityDemo';
-import ErrorHandlingDemo from './components/ErrorHandlingDemo';
-import UIPolishDemo from './components/UIPolishDemo';
+
+
 
 function App() {
   return (
@@ -26,13 +21,8 @@ function App() {
             <Route path="/upload" element={<UploadPage />} />
             <Route path="/agents" element={<BrowseAgentsPage />} />
             <Route path="/agents/:id" element={<AgentDetailPage />} />
-            <Route path="/api-example" element={<ApiExample />} />
-            <Route path="/agent-cards" element={<AgentCardDemo />} />
-            <Route path="/category-filter" element={<CategoryFilterDemo />} />
-            <Route path="/filter-integration" element={<FilterIntegrationDemo />} />
-            <Route path="/download-demo" element={<DownloadFunctionalityDemo />} />
-            <Route path="/error-demo" element={<ErrorHandlingDemo />} />
-            <Route path="/ui-demo" element={<UIPolishDemo />} />
+    
+
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </Layout>
