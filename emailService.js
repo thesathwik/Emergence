9999079f8a22ba23b1e42816ca588fae10c733ba
Sathwik Reddy <sathwikreddy@Mac.attlocal.net>
2025-08-21 13:34:35 -0400

    fix: correct nodemailer.createTransport method name

--- emailService.js
+++ emailService.js
@@ -13,7 +13,7 @@ const emailConfig = {
 };
 
 // Create transporter
-const transporter = nodemailer.createTransporter(emailConfig);
+const transporter = nodemailer.createTransport(emailConfig);
 
 // Generate verification token
 function generateVerificationToken() {
